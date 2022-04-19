4d060d00f4d4c5e92dde0e78b0c4bc81245c6616
Rob-O-Code <rpoleynick@gmail.com>
2022-04-19 15:59:23 -0400

    Recognize Nate's headphones

--- DoorIO.ino
+++ DoorIO.ino
@@ -2,15 +2,46 @@
 // Robbie Oleynick, Nate Reppucci, Cameron Pelletier //
 ///////////////////////////////////////////////////////
 
-#include "ePaper.h"
+#include <Arduino.h>
+#include "Bluetooth.h"
+#define PIR_PIN 14
+#define PHOTO_PIN 4
+#define BUZZ_PIN 25
 
-ePaper epd;
+MyBluetooth myBluetooth;
 
 void setup() {
   Serial.begin(115200);
-  epd.init();
+  pinMode(PIR_PIN, INPUT);
+  pinMode(PHOTO_PIN, INPUT);
+  pinMode(BUZZ_PIN, OUTPUT);
+
+  myBluetooth.init();
 }
 
+int timer = 0;
+
 void loop() {
-  
+  myBluetooth.testScan();
+  /*
+  static bool prevSense = digitalRead(PIR_PIN);
+  bool sense = digitalRead(PIR_PIN);
+  if (prevSense != sense) {
+    Serial.print("Now ");
+    Serial.println(sense ? "HIGH" : "LOW");
+    prevSense = sense;
+    if (sense) {
+      digitalWrite(BUZZ_PIN, HIGH);
+      timer = 20;
+    }
+  }
+  int value = analogRead(PHOTO_PIN);
+  //Serial.printf("%d\n", value);
+
+  if (timer > 0) {
+    timer--;
+  } else {
+    digitalWrite(BUZZ_PIN, LOW);
+  }
+  delay(10);*/
 }
--- bluetooth.h
+++ bluetooth.h
@@ -1,11 +1,31 @@
 #ifndef BLUETOOTH_H_
 #define BLUETOOTH_H_
 
+#include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 
+class MyBluetooth {
+  public:
+    MyBluetooth() {};
+    void init();
+    void testScan();
+  private:
+    static const int scanTime = 5;
+    BLEScan* pBLEScan;
+};
 
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    static std::string NatePhones = "94:db:56:02:61:6b";
+    static BLEAddress NateAddress = BLEAddress(NatePhones);
+    //Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    if (advertisedDevice.getAddress().equals(NateAddress)) {
+      Serial.printf("Nate RSSI: %d\n", advertisedDevice.getRSSI());
+    }
+  }
+};
 
 #endif
